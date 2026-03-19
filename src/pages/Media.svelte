087cf06ba6161e8224e34614a9616ed7c108b6ed
SPCOdesa <women.center.od@gmail.com>
2026-03-19 16:05:23 +0200

    Update Media.svelte

--- src/pages/Media.svelte
+++ src/pages/Media.svelte
@@ -4,10 +4,12 @@
   import Gallery from "../components/Gallery.svelte";
   import mediaJson from "../../content/data/media.json";
 
-  $: galleryImages = (mediaJson.images || []).map((img) => ({
-    src: img.src,
-    alt: $language === "uk" ? img.alt : img.altEn || img.alt,
-  }));
+  $: galleryImages = (mediaJson.images || [])
+    .map((img) => ({
+      src: img.src,
+      alt: $language === "uk" ? img.alt : img.altEn || img.alt,
+    }))
+    .reverse();
 </script>
 
 <Hero title={$t.media.hero.title} subtitle={$t.media.hero.subtitle} />
